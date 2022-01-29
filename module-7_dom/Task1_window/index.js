c6e9009c6c3c42e87227354d34fded7fd010ba41
Rosttyslav Kostenko <rostyslav_kostenko@epam.com>
2022-01-29 16:09:56 +0200

    added throttle func

--- module-7_dom/Task1_window/index.js
+++ module-7_dom/Task1_window/index.js
@@ -5,3 +5,17 @@ function getWindowSize() {
   heightElement.innerText = window.innerHeight;
   widthElement.innerText = window.innerWidth;
 }
+
+function throttle(callback, time) {
+  return function () {
+    let previousCall = this.lastCall;
+    this.lastCall = Date.now();
+    if (previousCall === undefined || this.lastCall - previousCall > time) {
+      callback();
+    }
+  };
+}
+
+function insertData() {
+  throttle(getWindowSize, 500);
+}
